584f49a38d42ab439559b7a970fc233c975e7a05
oxdjww <oxdjww@gmail.com>
2024-04-01 21:21:56 +0900

    add calculator

---
+++ calculator.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+void add()
+{
+}
+void sub()
+{
+}
+void mul()
+{
+}
+void div()
+{
+}
+
+int main(void)
+{
+    while (1)
+    {
+        printf("===== 계산기 프로그램 =====\n");
+        printf("[*] 연산 선택: ");
+        int ipt = getchar();
+        int a = getchar();
+        int b = getchar();
+        switch (ipt)
+        {
+        case '+':
+            printf("[*] 더하기를 진행합니다.\n");
+            add(a, b);
+        case '-':
+            printf("[*] 빼기를 진행합니다.\n");
+            sub(a, b);
+        case '*':
+            printf("[*] 곱하기를 진행합니다.\n");
+            mul(a, b);
+        case '/':
+            printf("[*] 나누기를 진행합니다.\n");
+            div(a, b);
+        }
+    }
+    return 0;
+}
